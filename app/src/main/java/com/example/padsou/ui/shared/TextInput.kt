bb0e21d6e5b4e0e73ad4f5a44355404fef9a57b7
JoMobile <lapinette2002@ymail.com>
2022-12-16 15:00:15 +0100

    Style du textInput

--- app/src/main/java/com/example/padsou/ui/shared/TextInput.kt
+++ app/src/main/java/com/example/padsou/ui/shared/TextInput.kt
@@ -2,20 +2,33 @@ package com.example.padsou.ui.shared
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.padsou.ui.theme.BackgroundWhite
 
 
 @Composable
-fun TextInput(placeholder:String){
+fun TextInput(placeholder:String, height: Dp = 56.dp){
     var inputText by remember { mutableStateOf("") }
-    Column() {
-        OutlinedTextField(value = inputText, onValueChange = { inputText = it }, placeholder = {Text(placeholder)})
-    }
+    OutlinedTextField(value = inputText, onValueChange = { inputText = it }, placeholder = {
+        Text(placeholder)
+    }, modifier = Modifier.fillMaxWidth()
+        .height(height)
+        .padding(horizontal = 31.dp)
+        .clip(RoundedCornerShape(15.dp))
+        .background(Color.White)
+    )
 }
 
 
