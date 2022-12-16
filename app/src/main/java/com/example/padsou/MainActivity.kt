cf80ed57d2c72073e52dbd7c385bfa1d89dac348
RomainLENTZ <romain.lentz74@gmail.com>
2022-12-16 10:51:37 +0100

    ADD [General] Connexion firebase

--- app/src/main/java/com/example/padsou/MainActivity.kt
+++ app/src/main/java/com/example/padsou/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.padsou
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -11,33 +13,41 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.padsou.ui.theme.PadsouTheme
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : ComponentActivity() {
+    val db = Firebase.firestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PadsouTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            Text("test")
+            Button(onClick = {
+                val user = hashMapOf(
+                    "email" to "romain.lentz74@gmail.com",
+                    "password" to "test"
+                )
+
+                db.collection("users")
+                    .add(user)
+                    .addOnSuccessListener { documentReference ->
+                        Log.d("success", "DocumentSnapshot added with ID: ${documentReference.id}")
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w("error", "Error adding document", e)
+                    }
+            }) {
+
             }
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    PadsouTheme {
-        Greeting("Android")
-    }
+
 }
