0869b87e7318ad4e1f17a55908ad6c7d49e6704f
JoMobile <lapinette2002@ymail.com>
2022-12-16 16:28:15 +0100

    Création des petits marqueurs de progressio

---
+++ app/src/main/java/com/example/padsou/ui/add_plan/AddPlanStepMarker.kt
@@ -0,0 +1,32 @@
+package com.example.padsou.ui.shared
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.padsou.ui.theme.MainPurple
+
+@Composable
+fun AddPlanStepMarker(filled:Boolean = false){
+
+    Box(
+        Modifier
+            .height(7.dp)
+            .width(46.dp)
+            .then(if(!filled)
+                Modifier.background(color = Color(0xFFBABFCD).copy(alpha = 0.4f), shape = RoundedCornerShape(20.dp) )
+            else
+                Modifier.background(color = MainPurple, shape = RoundedCornerShape(20.dp) )
+            )
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultOnBoardingPreview() {
+    AddPlanStepMarker()
+}
