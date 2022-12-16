98f1a29b90790346e94ea6a9a99ac1646d1e416c
JoMobile <lapinette2002@ymail.com>
2022-12-16 15:17:40 +0100

    Fix border du textInput

--- app/src/main/java/com/example/padsou/ui/shared/TextInput.kt
+++ app/src/main/java/com/example/padsou/ui/shared/TextInput.kt
@@ -1,6 +1,8 @@
 package com.example.padsou.ui.shared
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -8,6 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -16,6 +19,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.padsou.ui.theme.BackgroundWhite
+import com.example.padsou.ui.theme.MainPurple
 
 
 @Composable
@@ -23,11 +27,13 @@ fun TextInput(placeholder:String, height: Dp = 56.dp){
     var inputText by remember { mutableStateOf("") }
     OutlinedTextField(value = inputText, onValueChange = { inputText = it }, placeholder = {
         Text(placeholder)
-    }, modifier = Modifier.fillMaxWidth()
+    },
+        modifier = Modifier.fillMaxWidth()
         .height(height)
         .padding(horizontal = 31.dp)
-        .clip(RoundedCornerShape(15.dp))
-        .background(Color.White)
+        .background(Color.White),
+        colors = TextFieldDefaults.outlinedTextFieldColors(focusedBorderColor = MainPurple,unfocusedBorderColor = Color.White),
+        shape = RoundedCornerShape(15.dp)
     )
 }
 
