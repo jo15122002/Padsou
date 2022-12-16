aa9a7199caff78b3810e76d7b17ca6fc1e7a26d5
JoMobile <lapinette2002@ymail.com>
2022-12-16 13:59:38 +0100

    Renommage de la vue

--- app/src/main/java/com/example/padsou/ui/add_plan/AddPlanView.kt
+++ app/src/main/java/com/example/padsou/ui/add_plan/AddPlanView.kt
@@ -1,31 +1,36 @@
 package com.example.padsou.ui.add_plan
 
-import android.os.Bundle
-import android.util.Log
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.padsou.ui.shared.TopPageTitle
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 @Composable
-fun AddPlan(){
+fun AddPlanView(){
     val db = Firebase.firestore
-    Column(modifier = Modifier.background(Color(0xFF5f67ea))) {
-        Text(text = "Ajouter un bon plan")
+    Column(modifier =
+    Modifier
+        .background(Color(0xFF5f67ea))
+        .fillMaxWidth()
+        .fillMaxHeight()
+    ) {
+        TopPageTitle("Ajouter", "Un bon plan pour en faire profiter les autres")
+        Column() {
+
+        }
     }
 }
 
 
 @Preview(showBackground = true)
 @Composable
-fun AddPlanPreview() {
-
+fun AddPlanViewPreview() {
+    AddPlanView()
 }
