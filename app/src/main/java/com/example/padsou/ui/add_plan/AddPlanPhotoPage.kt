79ae4a6ce0a82cb41ada4be6fe6063d7f5cb2c2f
JoMobile <lapinette2002@ymail.com>
2023-01-16 10:06:19 +0100

    Centre et ajout dans le layout

--- app/src/main/java/com/example/padsou/ui/add_plan/AddPlanPhotoPage.kt
+++ app/src/main/java/com/example/padsou/ui/add_plan/AddPlanPhotoPage.kt
@@ -1,10 +1,11 @@
 package com.example.padsou.ui.add_plan
 
-import android.view.Gravity
+import android.widget.Space
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -12,19 +13,27 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.padsou.ui.shared.AddPlanStepMarker
-import com.example.padsou.ui.shared.TextInputWithTitle
 import com.example.padsou.ui.shared.TopPageTitle
 import com.example.padsou.ui.theme.BackgroundWhite
 import com.example.padsou.ui.theme.MainPurple
 import coil.compose.AsyncImage
+import com.example.padsou.ui.shared.Layout
+import com.google.common.cache.Weigher
 
 @Composable
 fun AddPlanPhotoPage(navController : NavHostController){
+    Layout(content = { AddPlanPhotoContentPage(navController = navController) }, navController = navController)
+}
+
+@Composable
+fun AddPlanPhotoContentPage(navController : NavHostController){
     Column(modifier =
     Modifier
         .background(MainPurple)
@@ -50,10 +59,10 @@ fun AddPlanPhotoPage(navController : NavHostController){
             Column(modifier = Modifier
                 .fillMaxSize()
                 .align(Alignment.CenterHorizontally)
-                .background(Color.Black)
                 ,
-                verticalArrangement = Arrangement.SpaceBetween
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
+                Text(text = "PHOTO DU BON PLAN", style = MaterialTheme.typography.h4, modifier = Modifier.padding(top = 20.dp, bottom = 26.dp))
                 Box(modifier = Modifier
                     .background(MainPurple, shape = RoundedCornerShape(18.dp))
                     .width(174.dp)
@@ -63,13 +72,17 @@ fun AddPlanPhotoPage(navController : NavHostController){
                 ){
                     AsyncImage(model = "https://cdn-icons-png.flaticon.com/512/17/17128.png", contentDescription = "Ajouter une photo", modifier = Modifier.width(65.dp))
                 }
+                Spacer(modifier = Modifier.weight(1f))
                 Box(modifier = Modifier
-                    .clickable { }
                     .background(MainPurple, shape = RoundedCornerShape(18.dp))
-                    .padding(horizontal = 133.dp, vertical = 18.dp)
+                    .width(313.dp)
+                    .height(70.dp)
+                    ,
+                    contentAlignment = Alignment.Center
                     ) {
                     Text("Ajouter ce bon plan", color = Color.White, style = MaterialTheme.typography.h3)
                 }
+                Spacer(modifier = Modifier.height(32.dp))
             }
         }
     }
