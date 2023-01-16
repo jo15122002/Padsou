6625f1618a1567f561aa9cba98118728f0ad384d
Lucas Leperlier <lucas.leperlier@gmail.com>
2023-01-16 14:32:46 +0100

    home page done w plans and category from db 🤯🤯

--- app/src/main/java/com/example/padsou/data/database/Database.kt
+++ app/src/main/java/com/example/padsou/data/database/Database.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import com.example.padsou.data.models.Category
+import com.example.padsou.data.models.Plan
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
@@ -23,7 +24,6 @@ object Database{
 
     fun getAllCategory(onSuccess : (MutableList<Category>)->Unit) = GlobalScope.async {
         val items = mutableListOf<Category>()
-
         db.collection("category")
             .get()
             .addOnSuccessListener { result ->
@@ -31,6 +31,21 @@ object Database{
                     val localCategory = document.toObject<Category>()
                     items.add(localCategory)
                     Log.d("ViewModel", "DocumentSnapshot localCategory: $localCategory")
+                }
+                onSuccess(items)
+            }
+            .addOnFailureListener { }
+    }
+
+    fun getAllPlan(onSuccess : (MutableList<Plan>)->Unit) = GlobalScope.async {
+        val items = mutableListOf<Plan>()
+        db.collection("plans")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    val localPlan = document.toObject<Plan>()
+                    items.add(localPlan)
+                    Log.d("ViewModel", "DocumentSnapshot localPlan: $localPlan")
                 }
                 onSuccess(items)
             }
--- app/src/main/java/com/example/padsou/data/models/Plan.kt
+++ app/src/main/java/com/example/padsou/data/models/Plan.kt
@@ -1,10 +1,14 @@
 package com.example.padsou.data.models
 
-class Plan (var title: String, var description: String, var photoUrl: String,var logoUrl: String,  var link:String, var userId: String, var categoryId:String){
+class Plan (var id: String, var title: String, var description: String, var photoUrl: String,var logoUrl: String,  var link:String, var userId: String, var categoryId:String){
+
+    constructor() : this(defaultPlan().id, defaultPlan().title, defaultPlan().description, defaultPlan().photoUrl, defaultPlan().logoUrl, defaultPlan().link, defaultPlan().userId, defaultPlan().categoryId)
 
     companion object {
         fun defaultPlan(): Plan{
-            return Plan("Default plan",
+            return Plan(
+                (0..1000000).random().toString(),
+                "Default plan",
                 "2 tacos offerts",
                 "https://www.maspatule.com/blog/wp-content/uploads/2021/07/IMG_4190-2-800x1067.jpg",
                 "https://upload.wikimedia.org/wikipedia/commons/a/a3/Basic-Fit_logo.png",
@@ -13,6 +17,10 @@ class Plan (var title: String, var description: String, var photoUrl: String,var
                 "0"
             )
         }
+    }
+
+    override fun toString(): String {
+        return "Plan(title='$title')"
     }
 
 }
