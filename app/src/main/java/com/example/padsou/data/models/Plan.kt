9435f31c3b800bcee619b24508ef7d620fb7e7de
JoMobile <lapinette2002@ymail.com>
2023-01-17 09:55:12 +0100

    toutes les properties à null

--- app/src/main/java/com/example/padsou/data/models/Plan.kt
+++ app/src/main/java/com/example/padsou/data/models/Plan.kt
@@ -1,14 +1,15 @@
 package com.example.padsou.data.models
 
-class Plan (
-    var title: String,
-    var description: String,
-    var photoUrl: String?,
-    var logoUrl: String?,
-    var link:String,
-    var userId: String,
-    var categoryId:String?,
-    var base64Images:List<String>?){
+class Plan(
+    var title: String = "",
+    var description: String = "",
+    var photoUrl: String = "",
+    var logoUrl: String = "",
+    var link:String = "",
+    var userId: String = "",
+    var categoryId:String = "",
+    var base64Images:List<String> = listOf()
+    ){
 
     companion object {
         fun defaultPlan(): Plan{
