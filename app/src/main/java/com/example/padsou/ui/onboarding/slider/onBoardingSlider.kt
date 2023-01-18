178cbcfde9e6e9a9b6888bb601389bb776b2823f
JoMobile <lapinette2002@ymail.com>
2023-01-18 16:10:07 +0100

    Ajout des indicateurs du slider

--- app/src/main/java/com/example/padsou/ui/onboarding/slider/onBoardingSlider.kt
+++ app/src/main/java/com/example/padsou/ui/onboarding/slider/onBoardingSlider.kt
@@ -5,12 +5,16 @@ import com.example.padsou.R
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -20,13 +24,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
 
 
 @OptIn(ExperimentalPagerApi::class)
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun onBoardingSlider(){
-
+    val state = rememberPagerState()
     Column (
         modifier = Modifier
             .padding(horizontal = 65.dp),
@@ -35,14 +40,30 @@ fun onBoardingSlider(){
 
     )
     {
-        HorizontalPager(count = 3) { page ->
+        DotsIndactor(nb = 3, selected = state.currentPage)
+        HorizontalPager(count = 3, state = state) { page ->
             when(page){
-                0-> Image(painter = painterResource(R.drawable.screenshot_1), contentDescription = "", modifier = Modifier.height(300.dp).aspectRatio(1f).padding(10.dp).clip(
-                    RoundedCornerShape(20.dp)), alignment = Alignment.TopCenter , contentScale = ContentScale.Crop)
-                1-> Image(painter = painterResource(R.drawable.screenshot_2), contentDescription = "", modifier = Modifier.height(300.dp).aspectRatio(1f).padding(10.dp).clip(
-                    RoundedCornerShape(20.dp)), alignment = Alignment.TopCenter , contentScale = ContentScale.Crop)
-                2-> Image(painter = painterResource(R.drawable.screenshot_3), contentDescription = "", modifier = Modifier.height(300.dp).aspectRatio(1f).padding(10.dp).clip(
-                    RoundedCornerShape(20.dp)), alignment = Alignment.TopCenter , contentScale = ContentScale.Crop)
+                0-> Image(painter = painterResource(R.drawable.screenshot_1), contentDescription = "", modifier = Modifier
+                    .height(300.dp)
+                    .aspectRatio(1f)
+                    .padding(10.dp)
+                    .clip(
+                        RoundedCornerShape(20.dp)
+                    ), alignment = Alignment.TopCenter , contentScale = ContentScale.Crop)
+                1-> Image(painter = painterResource(R.drawable.screenshot_2), contentDescription = "", modifier = Modifier
+                    .height(300.dp)
+                    .aspectRatio(1f)
+                    .padding(10.dp)
+                    .clip(
+                        RoundedCornerShape(20.dp)
+                    ), alignment = Alignment.TopCenter , contentScale = ContentScale.Crop)
+                2-> Image(painter = painterResource(R.drawable.screenshot_3), contentDescription = "", modifier = Modifier
+                    .height(300.dp)
+                    .aspectRatio(1f)
+                    .padding(10.dp)
+                    .clip(
+                        RoundedCornerShape(20.dp)
+                    ), alignment = Alignment.TopCenter , contentScale = ContentScale.Crop)
             }
         }
         /*Row(
@@ -70,4 +91,38 @@ fun onBoardingSlider(){
 @Composable
 fun DefaultOnBoardingSliderPreview() {
     onBoardingSlider()
+}
+
+@Composable
+fun DotsIndactor(nb:Int, selected:Int){
+    LazyRow(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight(), horizontalArrangement = Arrangement.Center
+    ) {
+        items(nb){ index ->
+            if(index == selected){
+                Box(
+                    modifier = Modifier
+                        .height(5.dp)
+                        .width(25.dp)
+                        .clip(RoundedCornerShape(5.dp))
+                        .background(color = Color.White)
+                )
+            } else {
+                Box(
+                    modifier = Modifier
+                        .height(5.dp)
+                        .width(25.dp)
+                        .clip(RoundedCornerShape(5.dp))
+                        .alpha(0.5f)
+                        .background(color = Color.White)
+                )
+            }
+
+            if (index != nb - 1) {
+                Spacer(modifier = Modifier.padding(horizontal = 4.dp))
+            }
+        }
+    }
 }
