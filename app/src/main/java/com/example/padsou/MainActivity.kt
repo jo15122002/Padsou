2ccb97da644b86ea0364db8f7cf188c0048c6bd8
JoMobile <lapinette2002@ymail.com>
2023-01-18 16:10:37 +0100

    remove import inutilisés

--- app/src/main/java/com/example/padsou/MainActivity.kt
+++ app/src/main/java/com/example/padsou/MainActivity.kt
@@ -5,23 +5,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.padsou.data.models.User
-import com.example.padsou.ui.onboarding.onBoardingPage
 import com.example.padsou.ui.shared.PadsouNavHost
 import com.example.padsou.ui.theme.*
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ktx.database
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 
 class MainActivity : ComponentActivity() {
 
